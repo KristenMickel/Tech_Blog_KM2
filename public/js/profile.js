07ae87219052f18e3158de1eca9f21fd139020ae
unknown <mickel.kristen@gmail.com>
2022-12-17 16:22:52 -0500

    Fixed a couple of issues

--- public/js/profile.js
+++ public/js/profile.js
@@ -21,20 +21,20 @@ const newFormHandler = async (event) => {
   //   }
   // };
 
-      const response = await fetch(`/api/blogs`, {
-        method: 'POST',
-        body: JSON.stringify({ name, description }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create blog');
-      }
+  const response = await fetch(`/api/blogs`, {
+    method: 'POST',
+    body: JSON.stringify({ name, description }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create blog');
     }
+  }
   
   const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
